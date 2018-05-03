ee52e7baad3b025f4f99da877c9bc6be1cd0a62b
Chris Woodard <sciprojguy@gmail.com>
2018-05-03 11:18:27 -0400

    Initial Commit

---
+++ TruckStops/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TruckStops
+//
+//  Created by Chris Woodard on 5/3/18.
+//  Copyright © 2018 Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TruckStops/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TruckStops
+//
+//  Created by Chris Woodard on 5/3/18.
+//  Copyright © 2018 Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
